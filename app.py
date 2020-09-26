dc0cadbffa04a9a8b5b105131ba30fba83334a6d
Sidney Cheng <shuzen0920@gmail.com>
2020-09-26 14:14:59 +0800

    emoji

--- app.py
+++ app.py
@@ -43,13 +43,14 @@ def handle_message(event):
         sticker_id='1'
     )
 
-    emojis = [{"productId": "5ac1de17040ab15980c9b438",
+    emojis = [{"index":6,
+                "productId": "5ac1de17040ab15980c9b438",
                 "emojiId": "004"}
     ]
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text, emojis))
+        TextSendMessage(text=event.message.text, emojis=emojis))
         #sticker_message)
 
 if __name__ == "__main__":
